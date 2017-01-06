429b4be9e2b61f6675b6122e59debad2422d9e1e
Ben Brown <ben@xoxco.com>
2017-01-06 14:36:40 -0600

    remove unnecessary hostname in express

--- components/express_webserver.js
+++ components/express_webserver.js
@@ -9,7 +9,6 @@ module.exports = function(controller) {
     var webserver = express();
     webserver.use(bodyParser.json());
     webserver.use(bodyParser.urlencoded({ extended: true }));
-    webserver.use(express.static('public'));
 
     // import express middlewares that are present in /components/express_middleware
     var normalizedPath = require("path").join(__dirname, "express_middleware");
@@ -17,11 +16,12 @@ module.exports = function(controller) {
         require("./express_middleware/" + file)(webserver, controller);
     });
 
+    webserver.use(express.static('public'));
 
-    webserver.listen(process.env.PORT || 3000, process.env.HOSTNAME || null, function() {
 
-        debug('Express webserver configured and listening at ',
-            process.env.HOSTNAME || 'http://localhost/' + ':' + process.env.PORT || 3000);
+    webserver.listen(process.env.PORT || 3000, null, function() {
+
+        debug('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
 
     });
 
