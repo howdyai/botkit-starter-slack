37def1e25063a954d66b4ee5fd915950c0766f9f
Michael Nathanson <michael.nathanson@gmail.com>
2017-01-12 16:49:10 -0500

    Ensuring bot exists before having it say anything

--- components/plugin_wordhop.js
+++ components/plugin_wordhop.js
@@ -9,8 +9,29 @@ module.exports = function(controller) {
       controller.middleware.receive.use(wordhop.receive); 
       controller.middleware.send.use(wordhop.send);
       // Handle forwarding the messages sent by a human through your bot
-      wordhop.on('chat response', function (message) {
-          bot.say(message);
+      wordhop.on('chat response', function (message) {   
+          var team_id = message.team || null;
+          controller.findTeamById(team_id, function(err, team) {
+              if (err) {
+                  controller.log.error('Could not load team while processing webhook: ', err);
+                  return;
+              } else if (!team) {
+                  // if this is NOT a slack app, it is ok to spawn a generic bot
+                  // this is only likely to happen with custom slash commands
+                  return
+              } else {
+                  // spawn a bot
+                  bot = controller.spawn(team);
+  
+                  // Identify the bot from either team storage or identifyBot()
+                  bot.team_info = team;
+                  bot.identity = {
+                      id: team.bot.user_id,
+                      name: team.bot.name
+                  };
+                  bot.say(message);
+              }
+          });
       });
       
       controller.log.info('Thanks for using Wordhop.');
