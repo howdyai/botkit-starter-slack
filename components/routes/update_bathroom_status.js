971fe05d2195aca8ffc27506073647b32d249a3d
Yunchi Luo <mightyguava@gmail.com>
2017-04-26 22:07:27 -0400

    Add web endpoint

--- components/routes/update_bathroom_status.js
+++ components/routes/update_bathroom_status.js
@@ -20,4 +20,31 @@ module.exports = function(webserver, controller) {
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify({status: tracker.status}));
     });
+
+    webserver.get('/status', function(req, res) {
+      res.status(200);
+      res.setHeader('Content-Type', 'text/html');
+      res.send(`
+<html>
+  <head>
+    <style type="text/css">
+      body {
+        margin: 0 auto;
+        font-size: 40px;
+        font-weight: bold;
+      }
+      .status-available {
+        background-color: green;
+      }
+      .status-occupied {
+        background-color: yellow;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="status-${tracker.status}">${tracker.status}</div>
+  </body>
+</html>
+`)
+    });
 }
