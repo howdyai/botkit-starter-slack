1e91e7179d6da937cad16873090959803fb245b8
Ben Brown <ben@xoxco.com>
2017-09-11 09:19:44 -0500

    update for version 0.6 of botkit
    change this skill to be a middleware instead of an event handler.
    this separates the feature of updating the message with "YOu said ____" from the feature of hearing the button payload.

--- skills/interactve_messages.js
+++ skills/interactve_messages.js
@@ -1,48 +1,34 @@
 module.exports = function(controller) {
 
     // create special handlers for certain actions in buttons
-    // if the button action is 'action', trigger an event
     // if the button action is 'say', act as if user said that thing
-    controller.on('interactive_message_callback', function(bot, trigger) {
-
-
-        if (trigger.actions[0].name.match(/^action$/)) {
-            controller.trigger(trigger.actions[0].value, [bot, trigger]);
-            return false; // do not bubble event
-        }
-        if (trigger.actions[0].name.match(/^say$/)) {
-
-            var message = {
-                user: trigger.user,
-                channel: trigger.channel,
-                text: '<@' + bot.identity.id + '> ' + trigger.actions[0].value,
-                type: 'message',
-            };
-
-            var reply = trigger.original_message;
+    controller.middleware.receive.use(function(bot, message, next) {
+      if (message.type == 'interactive_message_callback') {
+        if (message.actions[0].name.match(/^say$/)) {
+            var reply = message.original_message;
 
             for (var a = 0; a < reply.attachments.length; a++) {
                 reply.attachments[a].actions = null;
             }
 
-            var person = '<@' + trigger.user.id + '>';
+            var person = '<@' + message.user + '>';
             if (message.channel[0] == 'D') {
                 person = 'You';
             }
 
             reply.attachments.push(
                 {
-                    text: person + ' said, ' + trigger.actions[0].value,
+                    text: person + ' said, ' + message.actions[0].value,
                 }
             );
 
-            bot.replyInteractive(trigger, reply);
-
-            controller.receiveMessage(bot, message);
-            return false; // do not bubble event
-        }
-
+            bot.replyInteractive(message, reply);
+  
+         }
+      }
+      
+      next();    
+      
     });
-
 
 }
