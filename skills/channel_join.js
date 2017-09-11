d1b7dea2c1cf3b3779bd93b769d5116e8d35420e
Ben Brown <ben@xoxco.com>
2017-09-11 09:34:30 -0500

    Update for botkit 0.6

--- skills/channel_join.js
+++ skills/channel_join.js
@@ -4,7 +4,7 @@ module.exports = function(controller) {
 
     controller.on('bot_channel_join', function(bot, message) {
 
-        controller.studio.run(bot, 'channel_join', message.user, message.channel).catch(function(err) {
+        controller.studio.run(bot, 'channel_join', message.user, message.channel, message).catch(function(err) {
             debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
         });
 
